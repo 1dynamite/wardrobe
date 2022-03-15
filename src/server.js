b56b69620efae8385895d2c7b2f440a04bf7d6cf
1dynamite <turaevshokhrukh7@gmail.com>
2022-03-15 16:45:00 +0500

    fix: ready production for deploy

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 //------------------------------------------------------------------------------------
-var webpack = require('webpack');
-var webpackConfig = require('../webpack.dev.config');
+/* var webpack = require("webpack");
+var webpackConfig = require("../webpack.dev.config");
 var compiler = webpack(webpackConfig);
 
-app.use(require("webpack-dev-middleware")(compiler, {
-    publicPath: webpackConfig.output.publicPath
-}));
-app.use(require("webpack-hot-middleware")(compiler));
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+  })
+);
+app.use(require("webpack-hot-middleware")(compiler)); */
 //------------------------------------------------------------------------------------
 
-app.use('/static', express.static(path.resolve(__dirname, '../dist')));
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
-app.get('/', function (req, res) {
-    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
-    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
-    res.send(contentFromHtmlFile);
+app.get("/", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function () {
-    console.log('Application is running on http://localhost:3000/');
+  console.log("Application is running on http://localhost:3000/");
 });
